4025a15dfd9164ac5e35f202dd53db3b0f457a5f
andrey <a.sh09@mail.ru>
2017-10-09 09:09:11 +0530

    init commit

---
+++ youlead_17_registration/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
